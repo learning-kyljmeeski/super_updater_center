134b6ed376ea6992626ac5f20d08468c48de9146
learning-kyljmeeski <learning.kyljmeeski@gmail.com>
2024-05-02 15:45:02 +0600

    asdkfja

--- fileservice/src/main/java/kg/superpuper/fileservice/FileserviceApplication.java
+++ fileservice/src/main/java/kg/superpuper/fileservice/FileserviceApplication.java
@@ -1,9 +1,12 @@
 package kg.superpuper.fileservice;
 
+import kg.superpuper.fileservice.config.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class FileserviceApplication {
 
 	public static void main(String[] args) {
--- fileservice/src/main/java/kg/superpuper/fileservice/config/StorageProperties.java
+++ fileservice/src/main/java/kg/superpuper/fileservice/config/StorageProperties.java
@@ -1,7 +1,9 @@
 package kg.superpuper.fileservice.config;
 
 import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@ConfigurationProperties("storage")
 @Getter
 public class StorageProperties {
     private String location = "upload-dir";
